a9377c6f5e0bbeeaad7655269055b82c8860d103
Sandbox <dmitry.matveev@jetbrains.com>
2020-05-11 13:57:27 +0200

    update text mmessage

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git!");
+            Console.WriteLine("Hello, Git!");
         }
     }
 }
